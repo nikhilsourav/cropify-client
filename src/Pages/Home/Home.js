f7e8eb5de379da705403ca53e6fadf341f597509
nikhilsourav <nikhil.rj900@gmai.com>
2022-04-27 10:07:13 +0530

    Added about section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,6 +7,7 @@ import { Button } from '@mui/material';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import './styles.css';
+import ModernFarming from '../../Assets/modernFarming.jpg';
 
 /************************************************************************************************
  *
@@ -35,7 +36,26 @@ const Home = () => {
       </section>
       {/*********************************** end Header Section ***********************************/}
 
-      
+      {/************************************** About section **************************************/}
+      <section className='about' id='about'>
+        <div className='about-container'>
+          <img src={ModernFarming} alt='' className='about-img' />
+          <div className='about-info'>
+            <span>About</span>
+            <p>
+              We use cutting-edge Artificial Intelligence and Machine Learning technologies to
+              assist you with directing through the whole framing process. Get informed decisions
+              about your farming strategy to figure out the demographics of your region, comprehend
+              the variables that influence your crop and keep them healthy for a marvellous yield.
+            </p>
+            <p>
+              We provide a crop recommendation system and a crop yield prediction system based on ML
+              algorithms to enhance your farming experience.
+            </p>
+          </div>
+        </div>
+      </section>
+      {/************************************ end About section ************************************/}
     </motion.div>
   );
 };
